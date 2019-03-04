d890ebbefa126783e5a6d5a2913404c510a7d0a9
FredJiang <jiangf@shinetechchina.com>
2019-03-04 16:09:34 +0800

    Set Auditing Questionnaires status

--- dist/main.js
+++ dist/main.js
@@ -1,6 +1,8 @@
 "use strict";
 function greeter(person) {
+    var nouse = "fred";
     return "Hello, " + person;
 }
 var user = "Jane User";
 document.body.innerHTML = greeter(user);
+//# sourceMappingURL=main.js.map
--- main.ts
+++ main.ts
@@ -1,7 +1,8 @@
+// fred comment
+
 function greeter(person: any) {
     return "Hello, " + person;
 }
 
 let user = "Jane User"; 
-
 document.body.innerHTML = greeter(user);
